97182f38b82b5fdd37336778b1cc72898d2c41d5
inglap-w10 <inglaph@gmail.com>
2024-07-09 19:55:19 +0200

    Se gregan migration V7 y V8 con Payload de cursos y usuarios

---
+++ src/main/resources/db/migration/V7__insert-table-cursos-payload.sql
@@ -0,0 +1,6 @@
+INSERT INTO cursos (nombre, categoria)
+VALUES
+  ('Desarrollo Web', 'Programación'),
+  ('Marketing Digital', 'Marketing'),
+  ('Diseño Gráfico', 'Diseño'),
+  ('Contabilidad', 'Administración');
---
+++ src/main/resources/db/migration/V8__insert-table-usuarios-payload.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios (nombre, email, password, id_perfil)
+VALUES
+  ('Luis Posada', 'luisposada@example.com', '$2y$10$KpnVOBDqdzeklE5uFt.ppOs5ujQV6qt.3mdDM7IxZtS.a14Zipdr6', 1),
+  ('Juan Perez', 'juanperez@example.com', '$2y$10$LkHH7yniulNB8P9gl.lGGehDKDof0QuIF/FQrDbRzt91Hw1ZOTPZK', 2),
+  ('Maria Garcia', 'mariagarcia@example.com', '$2y$10$mvuswbTglMWRBTdfHa.MAuCkBMVq8KXmCOsUruTPpc4Ejwd1REZEq', 2);
